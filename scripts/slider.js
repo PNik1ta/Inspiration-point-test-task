60ae52456c117b3751c3b4e6c6aa69045b1620a9
pnik1ta <nikitap0zd3y3v@gmail.com>
2023-08-21 12:56:44 +0300

    Fix sliders

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,38 +1,59 @@
-const sliderContainer = document.getElementById('slider-container');
-const sliderBlocks = document.getElementsByClassName('table-main__slide');
-const circles = document.getElementById('circles');
+function initializeSlider(sliderId, prevBtnId, nextBtnId, circlesId) {
+	const sliderContainer = document.getElementById(sliderId);
+	const sliderBlocks = sliderContainer.getElementsByClassName('table-main__slide');
+	const circles = document.getElementById(circlesId);
 
-let currentIndex = 0;
+	const nextButton = document.getElementById(nextBtnId);
+	const prevButton = document.getElementById(prevBtnId);
 
-function updateSliderPosition() {
-	const offset = currentIndex * -100;
-	sliderContainer.style.transform = `translateX(${offset}%)`;
-	removeCircleActiveClasses();
-	circles.children[currentIndex].classList.add('navigation__circle_active');
-}
+	let currentIndex = 0;
 
-function removeCircleActiveClasses() {
-	for (let i = 0; i < circles.children.length; i++) {
-		if (circles.children[i].classList.contains('navigation__circle_active')) {
-			circles.children[i].classList.remove('navigation__circle_active');
-			break;
+	function updateSliderPosition() {
+		const offset = currentIndex * -100;
+		sliderContainer.style.transform = `translateX(${offset}%)`;
+		removeCircleActiveClasses();
+		circles.children[currentIndex].classList.add('navigation__circle_active');
+	}
+
+	function removeCircleActiveClasses() {
+		for (let i = 0; i < circles.children.length; i++) {
+			if (circles.children[i].classList.contains('navigation__circle_active')) {
+				circles.children[i].classList.remove('navigation__circle_active');
+				break;
+			}
 		}
 	}
+
+	function nextSlide() {
+		if (currentIndex >= sliderBlocks.length) {
+			nextButton.classList.add('disabled');
+			return;
+		}
+
+		if (prevButton.classList.contains('disabled')) {
+			prevButton.classList.remove('disabled');
+		}
+		currentIndex = (currentIndex + 1) % sliderBlocks.length;
+		updateSliderPosition();
+
+	}
+
+	function prevSlide() {
+		if (currentIndex <= 0) {
+			prevButton.classList.add('disabled');
+			return;
+		}
+
+		if (nextButton.classList.contains('disabled')) {
+			nextButton.classList.remove('disabled');
+		}
+		currentIndex = (currentIndex - 1 + sliderBlocks.length) % sliderBlocks.length;
+		updateSliderPosition();
+	}
+
+	nextButton.addEventListener('click', nextSlide);
+	prevButton.addEventListener('click', prevSlide);
 }
 
-function nextSlide() {
-	currentIndex = (currentIndex + 1) % sliderBlocks.length;
-	console.log(currentIndex);
-	updateSliderPosition();
-}
-
-function prevSlide() {
-	currentIndex = (currentIndex - 1 + sliderBlocks.length) % sliderBlocks.length;
-	updateSliderPosition();
-}
-
-const nextButton = document.getElementById('btn-next');
-const prevButton = document.getElementById('btn-prev');
-
-nextButton.addEventListener('click', nextSlide);
-prevButton.addEventListener('click', prevSlide);
+initializeSlider('slider-container-1', 'btn-prev-1', 'btn-next-1', 'circles-1');
+initializeSlider('slider-container-2', 'btn-prev-2', 'btn-next-2', 'circles-2');
